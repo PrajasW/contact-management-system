b4bcb0c519015e8b29c6c2c7220c63d789abaf7f
Prajas <prajaswadekar@gmail.com>
2024-04-26 09:43:47 +0530

    deployed

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   return mongoose
-    .connect("mongodb://localhost:27017/contact-list")
+    .connect("mongodb+srv://user:userpass@cluster0.6ekge94.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log(`connection to database established...`))
     .catch((err) => console.log(err));
 };
